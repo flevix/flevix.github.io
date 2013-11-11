4e87cd86d10596a1ba0807d5a11f8de2d3410935
flevix.ru@gmail.com <flevix.ru@gmail.com>
2013-11-11 16:21:14 +0400

    return to the better constructor

--- js/jPlayerReady.js
+++ js/jPlayerReady.js
@@ -2,7 +2,10 @@
  * Created by flevix on 08.11.13.
  */
 $(document).ready(function(){
-
+    var cssSelector = {
+        jPlayer: "#jquery_jplayer_1",
+        cssSelectorAncestor: "#jp_container_1"
+    };
     var playlist = [];
     $.ajax({
         type: "GET",
@@ -19,22 +22,17 @@ $(document).ready(function(){
             }
         }
     });
-
-    var myPlaylist = new jPlayerPlaylist(
-        {
-            jPlayer: "#jquery_jplayer_1",
-            cssSelectorAncestor: "#jp_container_1"
-        },
-        playlist,
-        {
-            swfPath : "./jPlayer",
-            supplied : "mp3"
-        }
-    );
-
-    //myPlaylist.setPlaylist();
+    var options = {
+        swfPath : "./jPlayer",
+        supplied : "mp3"
+    };
+    var myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
     //---
-    var playlist = [];
+    var cssSelector2 = {
+        jPlayer: "#jquery_jplayer_2",
+        cssSelectorAncestor: "#jp_container_2"
+    };
+    var playlist2 = [];
     $.ajax({
         type: "GET",
         url: "./JSON/playlist.json",
@@ -43,22 +41,16 @@ $(document).ready(function(){
         success: function(data) {
             for (var i = 0; i < data["data"].tracks.length; i++) {
                 var song = data["data"].tracks[i];
-                playlist.push({
+                playlist2.push({
                     title:song.title,
                     mp3:song.audio
                 });
             }
         }
     });
-    var myPlaylist2 = new jPlayerPlaylist(
-        {
-            jPlayer: "#jquery_jplayer_2",
-            cssSelectorAncestor: "#jp_container_2"
-        },
-        playlist,
-        {
-            swfPath : "./jPlayer",
-            supplied : "mp3"
-        }
-    );
+    var options2 = {
+        swfPath : "./jPlayer",
+        supplied : "mp3"
+    };
+    var myPlaylist2 = new jPlayerPlaylist(cssSelector2, playlist2, options2);
 });
