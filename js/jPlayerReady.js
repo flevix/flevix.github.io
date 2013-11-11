fd384653ba98dfc66d57120c5f57b924f9d35187
flevix.ru@gmail.com <flevix.ru@gmail.com>
2013-11-11 15:42:37 +0400

    jquery to local file. $.getJSON rewrite to syncronized ajax

--- js/jPlayerReady.js
+++ js/jPlayerReady.js
@@ -2,57 +2,63 @@
  * Created by flevix on 08.11.13.
  */
 $(document).ready(function(){
-    var cssSelector = {
-        jPlayer: "#jquery_jplayer_1",
-        cssSelectorAncestor: "#jp_container_1"
-    };
-    var options = {
-        swfPath : "./jPlayer",
-        supplied : "mp3",
-        play : function(time) {
-            alert("Play!");
-            $(this).jPlayer("pauseOthers");
-        },
-        pause : function(time) {
-            alert("Pause!");
-        }
-    };
+
     var playlist = [];
-
-    var myPlaylist = new jPlayerPlaylist(cssSelector, playlist, options);
-
-    $.getJSON("./JSON/playlist.json",
-        function(data) {
+    $.ajax({
+        type: "GET",
+        url: "./JSON/playlist.json",
+        async: false,
+        dataType: "json",
+        success: function(data) {
             for (var i = 0; i < data["data"].tracks.length; i++) {
                 var song = data["data"].tracks[i];
-                myPlaylist.add({
-                    title: song.title,
-                    mp3: song.audio
+                playlist.push({
+                    title:song.title,
+                    mp3:song.audio
                 });
             }
         }
+    });
+
+    var myPlaylist = new jPlayerPlaylist(
+        {
+            jPlayer: "#jquery_jplayer_1",
+            cssSelectorAncestor: "#jp_container_1"
+        },
+        playlist,
+        {
+            swfPath : "./jPlayer",
+            supplied : "mp3"
+        }
     );
+
+    //myPlaylist.setPlaylist();
     //---
-    var cssSelector2 = {
-        jPlayer: "#jquery_jplayer_2",
-        cssSelectorAncestor: "#jp_container_2"
-    };
-    var options2 = {
-        swfPath : "./jPlayer",
-        supplied : "mp3"
-    };
-    var playlist2 = [];
-
-    var myPlaylist2 = new jPlayerPlaylist(cssSelector2, playlist2, options2);
-
-    $.getJSON("./JSON/playlist.json",
-        function(data) {
-            $.each(data["data"].tracks, function(index, value) {
-                myPlaylist2.add({
-                    title : value.title,
-                    mp3 : value.audio
+    var playlist = [];
+    $.ajax({
+        type: "GET",
+        url: "./JSON/playlist.json",
+        async: false,
+        dataType: "json",
+        success: function(data) {
+            for (var i = 0; i < data["data"].tracks.length; i++) {
+                var song = data["data"].tracks[i];
+                playlist.push({
+                    title:song.title,
+                    mp3:song.audio
                 });
-            })
+            }
+        }
+    });
+    var myPlaylist2 = new jPlayerPlaylist(
+        {
+            jPlayer: "#jquery_jplayer_2",
+            cssSelectorAncestor: "#jp_container_2"
+        },
+        playlist,
+        {
+            swfPath : "./jPlayer",
+            supplied : "mp3"
         }
     );
 });
